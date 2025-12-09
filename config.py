647cf0fcc6660e19edfdad3ad24cfb5a5d3e0b83
23it092@charusat.edu.in <23it092@charusat.edu.in>
2025-12-09 11:03:29 +0530

    Updated Configy.py

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-# The "Control Panel" (Settings)
+import os
+
+# Dynamic Path Determination
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "data", "raw")
+
+# Settings
 TICKER = "RELIANCE.NS"
 START_DATE = "2020-01-01"
 END_DATE = "2024-01-01"
-DATA_PATH =  "C:/Users/shrey/Downloads/SGP-II/data/raw"
 INITIAL_CASH = 100000.0
-COMMISSION = 0.001 # Brokerage commission per trade
+COMMISSION = 0.001
+
+
+
